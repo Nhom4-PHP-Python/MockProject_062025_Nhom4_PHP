277c6c01dceb5f21633ff390bb74633ddf8e5601
hoa1905 <hoaptpt.1905@gmail.com>
2025-07-12 15:12:18 +0700

    Chore: add Vite asset loading with @vite directive to app layout

--- resources/views/layouts/app/app.blade.php
+++ resources/views/layouts/app/app.blade.php
@@ -21,7 +21,7 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <!-- Bootstrap JS Bundle -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
     <link rel="stylesheet" href="{{ asset('css/language-switcher.css') }}">
     <style>
